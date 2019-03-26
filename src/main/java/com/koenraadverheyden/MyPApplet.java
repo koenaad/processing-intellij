9b14344369364788be87b89055c1ed4bfa67d263
Koenraad Verheyden <mail@koenraadverheyden.com>
2019-03-26 16:27:05 +0100

    simplify example sketch

--- src/main/java/com/koenraadverheyden/MyPApplet.java
+++ src/main/java/com/koenraadverheyden/MyPApplet.java
@@ -1,16 +1,11 @@
 package com.koenraadverheyden;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class MyPApplet extends PApplet {
 
-    public static PApplet getPApplet() {
-        return pApplet;
-    }
-
-    private static PApplet pApplet;
-
-    private BouncyImage kunlaBounce;
+    private PImage kunlaboraLogo;
 
     @Override
     public void settings() {
@@ -20,18 +15,40 @@ public class MyPApplet extends PApplet {
 
     @Override
     public void setup() {
-        pApplet = this;
-
         blendMode(BLEND);
 
-        kunlaBounce = new BouncyImage("kunlabora.png", width / 2f, height / 2f, 50, 50);
+        kunlaboraLogo = loadImage("kunlabora.png");
     }
 
     @Override
     public void draw() {
+        // clear window
         background(0xFF);
 
-        kunlaBounce.update();
-        kunlaBounce.draw(this);
+        int squareSize = 100;
+
+        // 1 param: gray scale
+        // 0x00 = black, 0xFF = white
+        stroke(0x00);
+        fill(0xCC);
+
+        rect(50, 50, squareSize, squareSize);
+
+        // 3 params: R, G, B
+        // 0x00 -> 0xFF = intensity
+        stroke(0xFF, 0x33, 0x70);
+        fill(0xFF, 0x33, 0x70);
+
+        rect(250, 50, squareSize, squareSize);
+
+        // 4 params: R, G, B, A
+        // 0x00 = transparent, 0xFF = solid
+        noStroke();
+        fill(0xFF, 0x33, 0x70, 0x77);
+
+        rect(25, 225, squareSize, squareSize);
+        rect(75, 275, squareSize, squareSize);
+
+        image(kunlaboraLogo, 250, 250, squareSize, squareSize);
     }
 }
